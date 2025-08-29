99fcbd41b6ccb9ec5ee9e61933f17df7cc5451fd
Will <billywells99@hotmail.com>
2025-08-29 14:16:54 +1000

    first unit testing

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+}
+
+func cleanInput(text string) []string {
+	var myslice []string
+	text = strings.ToLower(text)
+	words := strings.Fields(text)
+
+	for _, word := range words {
+		myslice = append(myslice, word)
+	}
+	return myslice
 }
